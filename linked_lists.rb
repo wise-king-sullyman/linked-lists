8bdbb2a8f9188f04de1679a49735b9a795798908
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2020-10-06 22:42:03 -0400

    Filled out LinkedList #prepend and #to_s, added attr_readers for class read methods

--- linked_lists.rb
+++ linked_lists.rb
@@ -2,25 +2,32 @@
 
 # Responsible for management of an instance of a linked list
 class LinkedList
+  attr_reader :head, :tail, :size
+
+  def initialize(head_value = nil, tail_value = nil)
+    @head = Node.new(head_value, @tail)
+    @tail = Node.new(tail_value, nil)
+    @size = 2
+  end
+
   def append(value)
+    @tail = Node.new(value)
+    @size += 1
   end
 
   def prepend(value)
-  end
-
-  def size
-  end
-
-  def head
-  end
-
-  def tail
+    old_head = Node.new(@head.value, @tail)
+    new_head = Node.new(value, old_head)
+    @head = new_head
+    @size += 1
   end
 
   def at(index)
+
   end
 
   def pop
+    @size -= 1
   end
 
   def contains?(value)
@@ -30,21 +37,24 @@ class LinkedList
   end
 
   def to_s
+    node = @head
+    while node
+      print "( #{node.value} ) -> "
+      node = node.next_node
+    end
   end
 end
 
 # Responsible for creating instances of nodes
 class Node
-  def initialize(value, next_node)
+  attr_reader :value, :next_node
+  def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
-
-  def value
-    @value
-  end
-
-  def next_node
-    @next_node
-  end
 end
+
+list = LinkedList.new(41, 99)
+list.prepend(13)
+puts list.size
+puts list
